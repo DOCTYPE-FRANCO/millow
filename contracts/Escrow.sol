fff397f6dad47beb9d67be4664e3eda4ef70df9a
Franco <ufrancis186@gmail.com>
2025-11-12 20:00:59 +0100

    initial from Franco

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,5 +10,16 @@ interface IERC721 {
 }
 
 contract Escrow {
+    address public nftAddress;
+    address public lender;
+    address public inspector;
+    address payable public seller;
+
+    constructor(address _nftAddress, address _lender, address _inspector, address payable _seller) {
+        nftAddress = _nftAddress;
+        lender = _lender;
+        inspector = _inspector;
+        seller = _seller;
+    }
 
 }
